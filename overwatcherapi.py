e2d445ce8cac55cc8a1ecb45b980ea6238530762
Alex Catanzaro <alexcatanzaro@Alexs-MacBook-Pro.local>
2016-11-03 20:59:55 -0500

    added username stuffz

--- overwatcherapi.py
+++ overwatcherapi.py
@@ -8,13 +8,17 @@ app = Flask(__name__)
 api = Api(app)
 
 class OverWatcher(Resource):
-    def get(self):
-        page = requests.get('https://playoverwatch.com/en-us/career/psn/lSN00KI')
+    def get(self, owUser):
+        list statBase[]
+        page = requests.get('https://playoverwatch.com/en-us/career/psn/' + owUser)
         tree = html.fromstring(page.content)
         stat = tree.xpath('//div[@class="card-content"]//text()')
-        return(stat)
+        stat1= tree.xpath('//div[@data-group-id="stats"]//text()')
+        statBase.append(stat)
+        statBase.append(stat1)
+        return(statBase)
 
-api.add_resource(OverWatcher, '/')
+api.add_resource(OverWatcher, '/<string:owUser>')
 
 if __name__ == '__main__':
     app.run(debug=True)
