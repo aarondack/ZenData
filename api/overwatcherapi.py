06703d54383b48f3372d762f86c10ebebe175102
Aaron Dack <adackbusiness@gmail.com>
2016-11-15 16:59:57 -0500

    pure flask

--- api/overwatcherapi.py
+++ api/overwatcherapi.py
@@ -1,57 +1,51 @@
-from flask import Flask
-from flask_restful import Resource, Api, abort
+from flask import Flask, render_template, jsonify, abort
 from competitive import build_competitive_average, build_combat_total, hero_list, api_fetch, build_top_heroes, user_achievements
 
-app = Flask(__name__)
-api = Api(app)
+app = Flask(__name__, template_folder="../client")
 
 headers = ['Combat', 'Assists', 'Best', 'Deaths', 'Match Awards', 'Game', 'Miscellaneous']
 hero_headers = ['Hero Specific','Combat', 'Assists', 'Best', 'Deaths', 'Match Awards', 'Game', 'Miscellaneous']
 
-class OverWatcher(Resource):
-    def get(self, owUser):
-        statBase = {}
-        tree = api_fetch(owUser)
-        stats = tree.xpath('//div[@id="competitive"]//div[@data-group-id="stats" and @data-category-id="0x02E00000FFFFFFFF"]//text()')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        total = build_combat_total(stats, headers)
-        averages = build_competitive_average(tree.xpath('//div[@id="competitive"]//ul//text()'))
+@app.route('/api/<string:owUser>', methods=['GET'])
+def get_combat_averages(owUser):
+    statBase = {}
+    tree = api_fetch(owUser)
+    stats = tree.xpath('//div[@id="competitive"]//div[@data-group-id="stats" and @data-category-id="0x02E00000FFFFFFFF"]//text()')
 
-        statBase['averages'] = averages
-        statBase['stats'] = total
-        return(statBase)
+    total = build_combat_total(stats, headers)
+    averages = build_competitive_average(tree.xpath('//div[@id="competitive"]//ul//text()'))
 
-class HeroData(Resource):
-    def get(self, owUser, hero):
-        tree = api_fetch(owUser)
-        abort_if_no_hero_hash(hero)
+    statBase['averages'] = averages
+    statBase['stats'] = total
+    return jsonify(statBase)
 
-        hash_hero = hero_list[hero]
-        hero_stat = tree.xpath('//div[@id="competitive"]//div[@data-group-id="stats" and @data-category-id="{}"]//text()'.format(hash_hero))
+@app.route('/api/<string:owUser>/<string:hero>', methods=['GET'])
+def get_hero_data(owUser, hero):
+    tree = api_fetch(owUser)
+    abort_if_no_hero_hash(hero)
 
-        format_stats = build_combat_total(hero_stat, hero_headers)
-        return format_stats
+    hash_hero = hero_list[hero]
+    hero_stat = tree.xpath('//div[@id="competitive"]//div[@data-group-id="stats" and @data-category-id="{}"]//text()'.format(hash_hero))
 
-class UserAchievements(Resource):
-    def get(self, owUser):
-        tree = api_fetch(owUser)
-        achievements = user_achievements(tree)
-        return achievements
+    format_stats = build_combat_total(hero_stat, hero_headers)
+    return jsonify(format_stats)
 
-class TopHeroes(Resource):
-    def get(self, owUser):
-        tree = api_fetch(owUser)
-        top_heroes = build_top_heroes(tree)
-        return top_heroes
+@app.route('/api/<string:owUser>/achievements', methods=['GET'])
+def get_user_achievements(owUser):
+    tree = api_fetch(owUser)
+    achievements = user_achievements(tree)
+    return jsonify(achievements)
+
+@app.route('/api/<string:owUser>/topheroes', methods=['GET'])
+def get_top_heroes(owUser):
+    tree = api_fetch(owUser)
+    top_heroes = build_top_heroes(tree)
+    return jsonify(top_heroes)
 
 def abort_if_no_hero_hash(hero):
     if hero not in hero_list:
-        abort(404, message="That hero does not exist")
-
-api.add_resource(OverWatcher, '/api/<string:owUser>')
-api.add_resource(HeroData, '/api/<string:owUser>/<string:hero>')
-api.add_resource(UserAchievements, '/api/<string:owUser>/achievements')
-api.add_resource(TopHeroes, '/api/<string:owUser>/topheroes')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        abort(404)
