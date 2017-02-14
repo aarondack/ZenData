ef2d439b8ef746131c048ab6ec9f0654116a2f7b
Aaron Dack <adackbusiness@gmail.com>
2017-02-14 18:30:06 -0500

    Gracefully edit home screen

--- client/src/containers/Overview/index.js
+++ client/src/containers/Overview/index.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import Panel from '../../components/Panel';
 import Dashboard from '../Dashboard';
+import { connect } from 'react-redux';
+import CircularProgress from 'material-ui/CircularProgress';
 import './Overview.css';
 
 class Overview extends React.Component {
   render() {
+    const { fetching } = this.props;
     return (
     <div>
       <Dashboard />
-      <div className="overview-container">
-        <Panel />
+      {
+        fetching ? 
+      <div className="loading">
+        <h3>Loading...</h3>
+        <CircularProgress size={80} thickness={5} />
       </div>
+      : 
+          <div className="overview-container">
+          <Panel />
+      </div>
+      }
     </div>
     );
   }
 }
 
-export default Overview;
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    fetching: state.dashboard.fetching,
+  }
+}
+
+export default connect(mapStateToProps, null)(Overview);
